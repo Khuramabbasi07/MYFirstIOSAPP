096d38e98570062bf6c03e2aa49ac65d8c948bec
Khuramabbasi07 <155641857+khuramabbasi@users.noreply.github.com>
2025-09-15 15:46:55 +0500

    Initial iOS app commit

---
+++ MyFirstiOSApp/ContentView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Khuram Bhai 👌, iOS App is Ready!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
